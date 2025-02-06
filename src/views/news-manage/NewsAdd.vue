1b0466d1c1ee4e3c6abd43bce750962f6a55310c
2021211001001008HuWenjie <1842250324@qq.com>
2025-02-06 22:22:02 +0800

    5th commit

--- src/views/news-manage/NewsAdd.vue
+++ src/views/news-manage/NewsAdd.vue
@@ -1,9 +1,133 @@
 <template>
-	<div>newsadd</div>
+	<div>
+		<el-page-header content="添加新闻" icon="" title="新闻/公告管理" />
+		
+<!-- 		newsFormRef 进行校验的ref引用对象
+		newsForm 表单每个域进行绑定值的响应式对象模型
+		newsFormRules 对表单制定规则 -->
+		<el-form
+			ref="newsFormRef"
+			style="max-width: 600px"
+			:model="newsForm"
+			:rules="newsFormRules"
+			label-width="auto"
+			class="demo-ruleForm"
+			:size="formSize"
+			status-icon
+		  >
+		  
+			<el-form-item label="标题" prop="title">
+				<el-input v-model="newsForm.title" />
+			</el-form-item>
+		
+			<el-form-item label="内容" prop="content">
+				<editor @event="handleChange"/>
+			</el-form-item>
+			
+			<el-form-item label="类别" prop="category">
+				  <el-select
+				        v-model="newsForm.category"
+				        placeholder="Select"
+				        style="width: 100%"
+				      >
+				        <el-option
+				          v-for="item in options"
+				          :key="item.value"
+				          :label="item.label"
+				          :value="item.value"
+				        />
+				      </el-select>
+			</el-form-item>
+			
+			<el-form-item label="封面" prop="cover">
+				<Upload :avatar = "newsForm.cover" @mikachange="handleUploadChange"/>
+			</el-form-item>
+			
+			<el-form-item>
+			  <el-button type="primary" @click="submitForm()">
+			    添加新闻
+			  </el-button>
+			</el-form-item>
+		</el-form>
+	</div>
 </template>
 
-<script>
+<script setup>
+import { ref,reactive } from 'vue'
+import { useRouter } from 'vue-router'
+import upload from '@/util/upload'
+import Upload from '@/components/upload/Upload'
+import editor from '@/components/editor/Editor'
+
+const router = useRouter()
+
+const newsFormRef = ref()
+const newsFormRules = reactive({
+	title:[
+		{ required: true, message: '请输入标题', trigger: 'blur' }
+	],
+	content:[
+		{ required: true, message: '请输入内容', trigger: 'blur' }
+	],
+	category:[
+		{ required: true, message: '请选择分类', trigger: 'blur' }
+	],
+	cover:[
+		{ required: true, message: '请上传封面', trigger: 'blur' }
+	],
+})
+
+//新闻类别
+const options = [
+	{
+		label:"最新动态",
+		value:1
+	},
+	{
+		label:"通知公告",
+		value:2
+	},
+	{
+		label:"警告",
+		value:3
+	}
+]
+
+const newsForm = reactive({
+	title:"",
+	content:"",
+	category:1,//1 最新动态，2 通知公告，3 警告
+	cover:"",
+	file:null,
+	isPublish:0//0 未发布，1 已经发布
+	 
+})
+
+//每次editor内容改变的回调函数
+const handleChange = (data) => {
+	newsForm.content = data
+}
+//将封面图片进行二进制转换给file
+const handleUploadChange = (file) => {
+	newsForm.cover = URL.createObjectURL(file);
+	newsForm.file = file;
+}
+//提交news表单校验通过提交给后端
+const submitForm = () => {
+	newsFormRef.value.validate(async (valid) => {
+		if(valid){
+			//提交数据到后端
+			console.log(newsForm)
+			const res = await upload("/adminapi/news/add",newsForm);	
+			router.push(`/news-manage/newslist`)
+
+		}
+	})
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
+.el-form{
+	margin-top:50px;
+}
 </style>
--- src/views/news-manage/NewsList.vue
+++ src/views/news-manage/NewsList.vue
@@ -1,9 +1,149 @@
 <template>
-	<div>newslist</div>
+	<div>
+		<el-card>
+			<el-page-header content="新闻列表" icon="" title="新闻/公告管理" />
+			
+			<el-table :data="tableData" style="width: 100%">
+				<el-table-column prop="title" label="标题" width="auto" />
+				
+				<el-table-column prop="category" label="分类" width="auto">
+					<template #default="scope">
+						<el-tag v-if="scope.row.category === 1" type="success">{{categoryFormat(scope.row.category)}}</el-tag>
+						<el-tag v-if="scope.row.category === 2" type="warning">{{categoryFormat(scope.row.category)}}</el-tag>
+						<el-tag v-if="scope.row.category === 3" type="danger">{{categoryFormat(scope.row.category)}}</el-tag>
+					</template>
+				</el-table-column>
+				
+				<el-table-column prop="editTime" label="更新时间" width="auto">
+					<template #default="scope">
+						{{formatTime.getTime(scope.row.editTime)}}
+					</template>
+				</el-table-column>
+				
+	<!-- 		通过:active-value="1"去自定义开关判定依据，如果绑定的值是1就打开，
+					:inactive-value="0"关闭 -->
+				<el-table-column prop="isPublish" label="是否发布" width="auto">
+					<template #default="scope">
+						<el-switch v-model="scope.row.isPublish" 
+						:active-value="1"
+						:inactive-value="0"
+						@change="handleSwitchChange(scope.row)"/>
+					</template>
+				</el-table-column>
+				
+				<el-table-column label="操作">
+				  <template #default="scope">
+					  
+					<el-button size="small" circle :icon="View" type="success" @click="handlePriview(scope.row)" />
+					
+					<el-button size="small" circle :icon="Edit"  @click="handleEdit(scope.row)" />
+					
+					<el-button size="small" circle :icon="Delete" type="danger" />
+					
+					<el-popconfirm title="你确定要删除吗?"
+					confirm-button-text="确定"
+					cancel-button-text="取消"	@confirm="handleDelete(scope.row)">
+						<template #reference>
+						  <el-button
+							size="small"
+							type="danger"
+						  >
+							删除
+						  </el-button>
+						</template>
+					  </el-popconfirm>
+				  </template>
+				</el-table-column>
+			</el-table>
+		</el-card>
+		
+		<el-dialog
+			v-model="dialogVisible"
+			title="预览新闻"
+			width="50%"
+		>
+		<div>
+			<h2>{{priviewData.title}}</h2>
+			<div style="font-size: 16px;color:gray;">{{formatTime.getTime(priviewData.editTime)}}</div>
+			
+			<el-divider>
+			  <el-icon><star-filled /></el-icon>
+			</el-divider>
+<!-- 			v-html最好别用，以免跨栈脚本攻击 -->
+			<div v-html="priviewData.content" class="htmlcontent"></div>
+			
+		</div>
+			
+			
+		</el-dialog>
+	</div>
 </template>
 
-<script>
+<script setup>
+import { ref,onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import axios from 'axios'
+import formatTime from '@/util/formatTime'
+import { Star,View,Edit,Delete,StarFilled } from '@element-plus/icons-vue'
+
+const tableData = ref([])
+const priviewData = ref({})
+const dialogVisible = ref(false)
+const router = useRouter()
+
+onMounted(() => {
+	getTableData()
+})
+
+const getTableData = async () => {
+	const res = await axios.get(`/adminapi/news/list`)
+	// console.log(res.data.data)
+	tableData.value = res.data.data
+}
+
+//格式化分类信息
+const categoryFormat = (category) => {
+	const arr = ["最新动态","通知公告","警告"]
+	return arr[category - 1]
+}
+
+//开关回调函数
+const handleSwitchChange = async (item) => {
+	await axios.put(`/adminapi/news/publish`,{id:item.id,isPublish:item.isPublish})
+	
+	await getTableData()
+}
+
+//预览回调函数
+const handlePriview = async (data) => {
+	dialogVisible.value = true
+	priviewData.value = data
+}
+
+//删除回调函数 
+const handleDelete = async (item) => {
+	await axios.delete(`/adminapi/news/list/${item.id}`)
+	getTableData()
+	
+}
+
+//编辑回调函数
+const handleEdit = async (item) => {
+	//跳转到/news-manage/editnews/:id
+	router.push(`/news-manage/editnews/${item.id}`)
+	
+	
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
+.el-table{
+	margin-top: 50px;
+}
+
+::v-deep .htmlcontent{
+	img{
+		max-width: 100%;
+	}
+}
 </style>
